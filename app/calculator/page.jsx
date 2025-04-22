9024fe0d293313854399c6e0b97a1dec760b6fc4
Amaya-Wickramaarachchi <itbnm-2211-0195@horizoncampus.edu.lk>
2025-04-22 20:16:05 +0530

    fix: recent issues

--- app/calculator/page.jsx
+++ app/calculator/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect } from 'react';
-import '../../styles.css';
+import '../styles.css';
 
 export default function Calculator() {
   const [courses, setCourses] = useState([]);
